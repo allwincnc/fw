0a0a1fbfba636bd6ff378ac35308be7cf340f517
MX_Master <mikhail@vydrenko.ru>
2021-05-14 19:58:17 +0600

    * CCU/CPUSCFG/PLL6 registers address fix

--- h6/sys.h
+++ h6/sys.h
@@ -6,19 +6,19 @@
 
 
 
-#define CPU_FREQ                450000000 // Hz
+#define CPU_FREQ                300000000 // Hz
 
-#define ARISC_FW_BASE           (0x00000000) // for ARM CPU it's 0x00040000
-#define ARISC_FW_SIZE           ((8+8+32)*1024)
+#define ARISC_FW_BASE           (0x00000000) // for ARM CPU it's 0x00100000
+#define ARISC_FW_SIZE           ((8+8+80)*1024)
 #define ARISC_SHM_SIZE          (4096)
 #define ARISC_SHM_BASE          (ARISC_FW_BASE + ARISC_FW_SIZE - ARISC_SHM_SIZE)
 
 /* ccm */
-#define CCM_BASE                0x01c20000
-#define R_PRCM_BASE             0x01f01400
+#define CCM_BASE                0x03001000
+#define R_PRCM_BASE             0x07010000
 
 /* pll6 */
-#define PLL6_CTRL_REG           (CCM_BASE + 0x0028)
+#define PLL6_CTRL_REG           (CCM_BASE + 0x0020)
 #define PLL6_CTRL_ENABLE        BIT(31)
 #define PLL6_CTRL_LOCK          BIT(28)
 #define PLL6_CTRL_BYPASS        BIT(25)
